44d414c1a243a81b0b038afd35ccc4de40398976
InverTome <104219631+invertome@users.noreply.github.com>
2023-04-19 13:58:12 -0600

    Update run_limmavoom.R

--- run_limmavoom.R
+++ run_limmavoom.R
@@ -4,6 +4,7 @@ library(edgeR)
 library(ggplot2)
 library(ggrepel)
 library(heatmap3)
+library(cowplot)
 
 # Read the count matrix and sample metadata files
 count_matrix <- read.csv("Count_Matrix.csv", row.names = 1)
@@ -76,6 +77,36 @@ ggplot(logcpm, aes(PC1, PC2, color = Tissue, shape = Injection, linetype = Feedi
   geom_text_repel(aes(label = rownames(logcpm)), force = 5)
 ggsave("PCA_plot.png", width = 10, height = 8)
 
+# Volcano plots for each comparison
+volcano_plots <- list()
+for (cname in colnames(contr.matrix)) {
+  res <- results_list[[cname]]
+  volcano_data <- data.frame(logFC = res$logFC, pvalue = res$P.Value, adjpvalue = res$adj.P.Val, Gene = rownames(res))
+  volcano_plot <- ggplot(volcano_data, aes(x = logFC, y = -log10(pvalue), color = adjpvalue < 0.05)) +
+    geom_point(alpha = 0.5) +
+    scale_color_manual(values = c("grey", "red"), guide = FALSE) +
+    theme_bw() +
+    xlab("log2 Fold Change") +
+    ylab("-log10 P-value") +
+    ggtitle(paste0("Volcano plot: ", cname)) +
+    geom_hline(yintercept = -log10(0.05), linetype = "dashed", color = "blue") +
+    geom_vline(xintercept = c(-1, 1), linetype = "dashed", color = "blue") +
+    geom_text_repel(aes(label = ifelse(adjpvalue < 0.05 & (abs(logFC) > 1), Gene, "")), size = 3, box.padding = 0.5)
+  volcano_plots[[cname]] <- volcano_plot
+  ggsave(paste0("volcano_plot_", cname, ".png"), volcano_plot, width = 10, height = 8)
+}
+
+# Bar plot showing the number of differentially expressed genes in each comparison
+num_de_genes <- sapply(results_list, function(x) sum(x$adj.P.Val < 0.05))
+bar_plot <- ggplot(data.frame(Comparison = names(num_de_genes), Num_DE_Genes = num_de_genes), aes(x = Comparison, y = Num_DE_Genes)) +
+  geom_bar(stat = "identity", fill = "steelblue") +
+  theme_bw() +
+  xlab("Comparison") +
+  ylab("Number of Differentially Expressed Genes") +
+  ggtitle("Number of Differentially Expressed Genes per Comparison") +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+ggsave("bar_plot_num_de_genes.png", bar_plot, width = 10, height = 8)
+
 # Heatmap visualization
 # Find top 50 differentially expressed genes across all contrasts
 top_genes <- unique(unlist(lapply(results_list, function(x) rownames(head(x, 50)))))
@@ -90,3 +121,4 @@ heatmap3(heatmap_data, ColSideColors = as.matrix(sample_metadata[, c("Tissue", "
 
 # Save the heatmap as a PNG file
 ggsave("heatmap.png", width = 1000, height = 1000, dpi = 150)
+
