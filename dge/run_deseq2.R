59aa7440a789a880b06fb2a80565378d17ad7a35
InverTome <104219631+invertome@users.noreply.github.com>
2023-04-21 10:29:41 -0600

    Update run_deseq2.R

--- dge/run_deseq2.R
+++ dge/run_deseq2.R
@@ -1,3 +1,9 @@
+if (!requireNamespace("BiocManager", quietly = TRUE))
+    install.packages("BiocManager")
+
+BiocManager::install(c("DESeq2", "tximport", "ggplot2", "pheatmap", "EnhancedVolcano"))
+
+
 # Load packages
 library(DESeq2)
 library(tximport)
@@ -18,21 +24,100 @@ dds <- DESeqDataSetFromTximport(txi, metadata, ~Tissue + Injection + Feeding)
 dds <- DESeq(dds)
 
 # Results
-res_deseq2 <- results(dds, contrast = c("Injection", "leptin", "saline"))
+res_EO_leptin_fooddep_vs_saline_fooddep <- results(dds, contrast = list("Injection_leptin_vs_saline", "Feeding_fooddep"), listValues = c(1,-1), test = "LRT")
+res_EO_leptin_adlib_vs_saline_adlib <- results(dds, contrast = list("Injection_leptin_vs_saline", "Feeding_adlib"), listValues = c(1,-1), test = "LRT")
+res_SM_leptin_fooddep_vs_saline_fooddep <- results(dds, contrast = list("Injection_leptin_vs_saline", "Feeding_fooddep"), listValues = c(-1,1), test = "LRT")
+res_SM_leptin_adlib_vs_saline_adlib <- results(dds, contrast = list("Injection_leptin_vs_saline", "Feeding_adlib"), listValues = c(-1,1), test = "LRT")
+
+# Save results
+dir.create("deseq2_output")
+write.csv(res_EO_leptin_fooddep_vs_saline_fooddep, file = "deseq2_output/EO_leptin_fooddep_vs_saline_fooddep.csv")
+write.csv(res_EO_leptin_adlib_vs_saline_adlib, file = "deseq2_output/EO_leptin_adlib_vs_saline_adlib.csv")
+write.csv(res_SM_leptin_fooddep_vs_saline_fooddep, file = "deseq2_output/SM_leptin_fooddep_vs_saline_fooddep.csv")
+write.csv(res_SM_leptin_adlib_vs_saline_adlib, file = "deseq2_output/SM_leptin_adlib_vs_saline_adlib.csv")
+
+# Save summaries
+summary_EO_leptin_fooddep_vs_saline_fooddep <- summary(res_EO_leptin_fooddep_vs_saline_fooddep)
+summary_EO_leptin_adlib_vs_saline_adlib <- summary(res_EO_leptin_adlib_vs_saline_adlib)
+summary_SM_leptin_fooddep_vs_saline_fooddep <- summary(res_SM_leptin_fooddep_vs_saline_fooddep)
+summary_SM_leptin_adlib_vs_saline_adlib <- summary(res_SM_leptin_adlib_vs_saline_adlib)
+
+write.csv(summary_EO_leptin_fooddep_vs_saline_fooddep, file = "deseq2_output/summary_EO_leptin_fooddep_vs_saline_fooddep.csv")
+write.csv(summary_EO_leptin_adlib_vs_saline_adlib, file = "deseq2_output/summary_EO_leptin_adlib_vs_saline_adlib.csv")
+write.csv(summary_SM_leptin_fooddep_vs_saline_fooddep, file = "deseq2_output/summary_SM_leptin_fooddep_vs_saline_fooddep.csv")
+write.csv(summary_SM_leptin_adlib_vs_saline_adlib, file = "deseq2_output/summary_SM_leptin_adlib_vs_saline_adlib.csv")
 
 # PCA plot
 plotPCA(dds)
+ggsave("deseq2_output/pca_plot.png")
 
-# Histogram
-hist(res_deseq2$pvalue, breaks = 50, col = "skyblue", border = "slateblue", main = "Histogram of p-values")
+# Histograms
+hist(res_EO_leptin_fooddep_vs_saline_fooddep$pvalue, breaks = 50, col = "skyblue", border = "slateblue", main = "Histogram of p-values (EO Leptin Fooddep vs Saline Fooddep)")
+ggsave("deseq2_output/histogram_EO_leptin_fooddep_vs_saline_fooddep.png")
+hist(res_EO_leptin_adlib_vs_saline_adlib$pvalue, breaks = 50, col = "skyblue", border = "slateblue", main = "Histogram of p-values (EO Leptin Adlib vs Saline Adlib)")
+ggsave("deseq2_output/histogram_EO_leptin_adlib_vs_saline_adlib.png")
+hist(res_SM_leptin_fooddep_vs_saline_fooddep$pvalue, breaks = 50, col = "skyblue", border = "slateblue", main = "Histogram of p-values (SM Leptin Fooddep vs Saline Fooddep)")
+ggsave("deseq2_output/histogram_SM_leptin_fooddep_vs_saline_fooddep.png")
+hist(res_SM_leptin_adlib_vs_saline_adlib$pvalue, breaks = 50, col = "skyblue", border = "slateblue", main = "Histogram of p-values (SM Leptin Adlib vs Saline Adlib)")
+ggsave("deseq2_output/histogram_SM_leptin_adlib_vs_saline_adlib.png")
 
-# Volcano plot
-EnhancedVolcano(res_deseq2, title = "Volcano plot")
+# Volcano plots
+vol_EO_leptin_fooddep_vs_saline_fooddep <- EnhancedVolcano(res_EO_leptin_fooddep_vs_saline_fooddep, title = "Volcano plot (EO Leptin Fooddep vs Saline Fooddep)", pCutoff = 0.05, FCcutoff = 1)
+ggsave("deseq2_output/volcano_EO_leptin_fooddep_vs_saline_fooddep.png", plot = vol_EO_leptin_fooddep_vs_saline_fooddep)
 
-# MA plot
-plotMA(res_deseq2, main = "MA plot")
+vol_EO_leptin_adlib_vs_saline_adlib <- EnhancedVolcano(res_EO_leptin_adlib_vs_saline_adlib, title = "Volcano plot (EO Leptin Adlib vs Saline Adlib)", pCutoff = 0.05, FCcutoff = 1)
+ggsave("deseq2_output/volcano_EO_leptin_adlib_vs_saline_adlib.png", plot = vol_EO_leptin_adlib_vs_saline_adlib)
 
-# Heatmap
-top_genes_deseq2 <- rownames(res_deseq2)[order(res_deseq2$pvalue)[1:50]]
-heatmap_data_deseq2 <- assay(vsd)[top_genes_deseq2,]
-pheatmap(heatmap_data_deseq2, cluster_rows = TRUE, cluster_cols = FALSE, show_rownames = FALSE, main = "Heatmap")
+vol_SM_leptin_fooddep_vs_saline_fooddep <- EnhancedVolcano(res_SM_leptin_fooddep_vs_saline_fooddep, title = "Volcano plot (SM Leptin Fooddep vs Saline Fooddep)", pCutoff = 0.05, FCcutoff = 1)
+ggsave("deseq2_output/volcano_SM_leptin_fooddep_vs_saline_fooddep.png", plot = vol_SM_leptin_fooddep_vs_saline_fooddep)
+
+vol_SM_leptin_adlib_vs_saline_adlib <- EnhancedVolcano(res_SM_leptin_adlib_vs_saline_adlib, title = "Volcano plot (SM Leptin Adlib vs Saline Adlib)", pCutoff = 0.05, FCcutoff = 1)
+ggsave("deseq2_output/volcano_SM_leptin_adlib_vs_saline_adlib.png", plot = vol_SM_leptin_adlib_vs_saline_adlib)
+
+# MA plots
+ma_EO_leptin_fooddep_vs_saline_fooddep <- plotMA(res_EO_leptin_fooddep_vs_saline_fooddep, main = "MA plot (EO Leptin Fooddep vs Saline Fooddep)")
+ggsave("deseq2_output/ma_EO_leptin_fooddep_vs_saline_fooddep.png", plot = ma_EO_leptin_fooddep_vs_saline_fooddep)
+
+ma_EO_leptin_adlib_vs_saline_adlib <- plotMA(res_EO_leptin_adlib_vs_saline_adlib, main = "MA plot (EO Leptin Adlib vs Saline Adlib)")
+ggsave("deseq2_output/ma_EO_leptin_adlib_vs_saline_adlib.png", plot = ma_EO_leptin_adlib_vs_saline_adlib)
+
+ma_SM_leptin_fooddep_vs_saline_fooddep <- plotMA(res_SM_leptin_fooddep_vs_saline_fooddep, main = "MA plot (SM Leptin Fooddep vs Saline Fooddep)")
+ggsave("deseq2_output/ma_SM_leptin_fooddep_vs_saline_fooddep.png", plot = ma_SM_leptin_fooddep_vs_saline_fooddep)
+
+ma_SM_leptin_adlib_vs_saline_adlib <- plotMA(res_SM_leptin_adlib_vs_saline_adlib, main = "MA plot (SM Leptin Adlib vs Saline Adlib)")
+ggsave("deseq2_output/ma_SM_leptin_adlib_vs_saline_adlib.png", plot = ma_SM_leptin_adlib_vs_saline_adlib)
+
+# Heatmaps
+log2FoldChange = lfcShrink(dds, coef = "Injection_leptin_vs_saline")
+top_genes <- head(order(log2FoldChange), 50)
+heatmap_data <- assay(dds)[ top_genes, ]
+pheatmap(heatmap_data, cluster_cols = TRUE, cluster_rows = TRUE)
+ggsave("deseq2_output/heatmap_top_genes.png")
+
+# Print summary
+cat("Generated files for DESeq2 analysis:\n")
+cat(paste("deseq2_output/EO_leptin_fooddep_vs_saline_fooddep.csv\n",
+          "deseq2_output/EO_leptin_adlib_vs_saline_adlib.csv\n",
+          "deseq2_output/SM_leptin_fooddep_vs_saline_fooddep.csv\n",
+          "deseq2_output/SM_leptin_adlib_vs_saline_adlib.csv\n",
+          "deseq2_output/summary_EO_leptin_fooddep_vs_saline_fooddep.csv\n",
+          "deseq2_output/summary_EO_leptin_adlib_vs_saline_adlib.csv\n",
+          "deseq2_output/summary_SM_leptin_fooddep_vs_saline_fooddep.csv\n",
+          "deseq2_output/summary_SM_leptin_adlib_vs_saline_adlib.csv\n",
+          "deseq2_output/pca_plot.png\n",
+          "deseq2_output/histogram_EO_leptin_fooddep_vs_saline_fooddep.png\n",
+          "deseq2_output/histogram_EO_leptin_adlib_vs_saline_adlib.png\n",
+          "deseq2_output/histogram_SM_leptin_fooddep_vs_saline_fooddep.png\n",
+          "deseq2_output/histogram_SM_leptin_adlib_vs_saline_adlib.png\n",
+          "deseq2_output/volcano_EO_leptin_fooddep_vs_saline_fooddep.png\n",
+          "deseq2_output/volcano_EO_leptin_adlib_vs_saline_adlib.png\n",
+          "deseq2_output/volcano_SM_leptin_fooddep_vs_saline_fooddep.png\n",
+          "deseq2_output/volcano_SM_leptin_adlib_vs_saline_adlib.png\n",
+          "deseq2_output/ma_EO_leptin_fooddep_vs_saline_fooddep.png\n",
+          "deseq2_output/ma_EO_leptin_adlib_vs_saline_adlib.png\n",
+          "deseq2_output/ma_SM_leptin_fooddep_vs_saline_fooddep.png\n",
+          "deseq2_output/ma_SM_leptin_adlib_vs_saline_adlib.png\n",
+          "deseq2_output/heatmap_top_genes.png\n",
+          sep = ""))
+
+
