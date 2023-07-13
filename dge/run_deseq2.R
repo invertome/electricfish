695be221a68fc429248956be11f966fa2a46eed2
InverTome <104219631+invertome@users.noreply.github.com>
2023-07-13 10:05:23 -0600

    Update run_deseq2.R
    more contrasts

--- dge/run_deseq2.R
+++ dge/run_deseq2.R
@@ -49,13 +49,19 @@ dds <- DESeq(dds)
 # Define contrasts
 contrasts <- list(
   c("condition", "EO_saline_fooddep", "EO_saline_adlib"),
-  c("condition", "EO_saline_adlib", "SM_saline_fooddep"),
-  c("condition", "EO_saline_fooddep", "SM_saline_fooddep"),
+  c("condition", "SM_saline_fooddep", "EO_saline_adlib"),
+  c("condition", "SM_saline_fooddep", "EO_saline_fooddep"),
   c("condition", "EO_leptin_adlib", "EO_saline_adlib"),
-  c("condition", "EO_leptin_adlib", "SM_leptin_adlib"),
+  c("condition", "SM_leptin_adlib", "EO_leptin_adlib"),
   c("condition", "EO_leptin_fooddep", "EO_saline_fooddep"),
-  c("condition", "EO_leptin_fooddep", "SM_leptin_fooddep"),
-  c("condition", "EO_leptin_fooddep", "EO_leptin_adlib")
+  c("condition", "SM_leptin_fooddep", "EO_leptin_fooddep"),
+  c("condition", "EO_leptin_fooddep", "EO_leptin_adlib"),
+  c("condition", "SM_saline_adlib", "EO_saline_adlib"),
+  c("condition", "SM_leptin_adlib", "EO_leptin_adlib"),
+  c("condition", "SM_saline_adlib", "SM_leptin_adlib"),
+  c("condition", "SM_saline_fooddep", "SM_leptin_fooddep"),
+  c("condition", "EO_saline_adlib", "EO_saline_fooddep"),
+  c("condition", "EO_leptin_adlib", "EO_leptin_fooddep")
 )
 
 # Apply contrasts and get results
@@ -66,14 +72,21 @@ res_list <- lapply(contrasts, function(cntrst) {
 
 names(res_list) <- c(
   "EO_saline_fooddep_vs_EO_saline_adlib",
-  "EO_saline_adlib_vs_SM_saline_fooddep",
-  "EO_saline_fooddep_vs_SM_saline_fooddep",
+  "SM_saline_fooddep_vs_EO_saline_adlib",
+  "SM_saline_fooddep_vs_EO_saline_fooddep",
   "EO_leptin_adlib_vs_EO_saline_adlib",
-  "EO_leptin_adlib_vs_SM_leptin_adlib",
+  "SM_leptin_adlib_vs_EO_leptin_adlib",
   "EO_leptin_fooddep_vs_EO_saline_fooddep",
-  "EO_leptin_fooddep_vs_SM_leptin_fooddep",
-  "EO_leptin_fooddep_vs_EO_leptin_adlib"
+  "SM_leptin_fooddep_vs_EO_leptin_fooddep",
+  "EO_leptin_fooddep_vs_EO_leptin_adlib",
+  "SM_saline_adlib_vs_EO_saline_adlib",
+  "SM_leptin_adlib_vs_EO_leptin_adlib",
+  "SM_saline_adlib_vs_SM_leptin_adlib",
+  "SM_saline_fooddep_vs_SM_leptin_fooddep",
+  "EO_saline_adlib_vs_EO_saline_fooddep",
+  "EO_leptin_adlib_vs_EO_leptin_fooddep"
 )
+
 
 
 # Save results
